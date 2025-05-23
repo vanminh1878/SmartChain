7bd704bf1746f30e3f7b62b76b937597a47dfb92
vanminh1878 <vanminh130804@gmail.com>
2025-05-23 22:38:11 +0700

    BE: Xây StockIntakeDetailsRepository

---
+++ BE/SmartChain.Infrastructure/StockIntakeDetail/Persistence/StockIntakeDetailsRepository.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SmartChain.Application.Common.Interfaces;
+using SmartChain.Domain.StockIntake;
+using SmartChain.Infrastructure.Common.Persistence;
+
+namespace SmartChain.Infrastructure.Persistence.Repositories;
+
+public class StockIntakeDetailsRepository : IStockIntakeDetailsRepository
+{
+    private readonly AppDbContext _context;
+
+    public StockIntakeDetailsRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(StockIntakeDetail stockIntakeDetail, CancellationToken cancellationToken)
+    {
+        await _context.StockIntakeDetails.AddAsync(stockIntakeDetail, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task<StockIntakeDetail?> GetByIdAsync(Guid stockIntakeDetailId, CancellationToken cancellationToken)
+    {
+        return await _context.StockIntakeDetails
+            .FirstOrDefaultAsync(sid => sid.Id == stockIntakeDetailId, cancellationToken);
+    }
+
+    public async Task<List<StockIntakeDetail>> ListByStockIntakeIdAsync(Guid stockIntakeId, CancellationToken cancellationToken)
+    {
+        return await _context.StockIntakeDetails
+            .Where(sid => sid.Id == stockIntakeId)
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<List<StockIntakeDetail>> ListByProductIdAsync(Guid productId, CancellationToken cancellationToken)
+    {
+        return await _context.StockIntakeDetails
+            .Where(sid => sid.ProductId == productId)
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task RemoveAsync(StockIntakeDetail stockIntakeDetail, CancellationToken cancellationToken)
+    {
+        _context.StockIntakeDetails.Remove(stockIntakeDetail);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task RemoveRangeAsync(List<StockIntakeDetail> stockIntakeDetails, CancellationToken cancellationToken)
+    {
+        _context.StockIntakeDetails.RemoveRange(stockIntakeDetails);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task UpdateAsync(StockIntakeDetail stockIntakeDetail, CancellationToken cancellationToken)
+    {
+        _context.StockIntakeDetails.Update(stockIntakeDetail);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
--- BE/SmartChain.Infrastructure/obj/Debug/net6.0/SmartChain.Infrastructure.AssemblyInfo.cs
+++ BE/SmartChain.Infrastructure/obj/Debug/net6.0/SmartChain.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartChain.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+248be6eb94f515c48120a67d30a1a9a28da12e3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49bcede102e12a27ee4d4540b3750ecd1fd7638c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartChain.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartChain.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
