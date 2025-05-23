59adabcf08f3c7515dfb2eb778ceeaa9a39ad888
vanminh1878 <vanminh130804@gmail.com>
2025-05-23 21:53:25 +0700

    BE: Xây ISchedulesRepository

---
+++ BE/SmartChain.Application/Common/Interfaces/ISchedulesRepository.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using SmartChain.Domain.Schedule;
+
+namespace SmartChain.Application.Common.Interfaces;
+
+public interface ISchedulesRepository
+{
+    Task AddAsync(Schedule schedule, CancellationToken cancellationToken);
+    Task<Schedule?> GetByIdAsync(Guid scheduleId, CancellationToken cancellationToken);
+    Task<List<Schedule>> ListByEmployeeIdAsync(Guid employeeId, CancellationToken cancellationToken);
+    Task<List<Schedule>> ListByDateRangeAsync(DateTime startDate, DateTime endDate, CancellationToken cancellationToken);
+    Task RemoveAsync(Schedule schedule, CancellationToken cancellationToken);
+    Task RemoveRangeAsync(List<Schedule> schedules, CancellationToken cancellationToken);
+    Task UpdateAsync(Schedule schedule, CancellationToken cancellationToken);
+}
--- BE/SmartChain.Application/obj/Debug/net6.0/SmartChain.Application.AssemblyInfo.cs
+++ BE/SmartChain.Application/obj/Debug/net6.0/SmartChain.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartChain.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0bc9ed3d0b1208ebeb5798c1a5b264fdb473ffc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+158c5d303374f05167a8d92ebb64afc62c6bc2d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartChain.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartChain.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
