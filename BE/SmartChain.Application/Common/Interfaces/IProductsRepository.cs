6ea1f73e5ebd324ff12e2294275aed0d2b59d6f0
vanminh1878 <vanminh130804@gmail.com>
2025-05-23 21:51:03 +0700

    BE: Xây IProductsRepository

---
+++ BE/SmartChain.Application/Common/Interfaces/IProductsRepository.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using SmartChain.Domain.Product;
+
+namespace SmartChain.Application.Common.Interfaces;
+
+public interface IProductsRepository
+{
+    Task AddAsync(Product product, CancellationToken cancellationToken);
+    Task<Product?> GetByIdAsync(Guid productId, CancellationToken cancellationToken);
+    Task<List<Product>> ListByStoreIdAsync(Guid storeId, CancellationToken cancellationToken);
+    Task<List<Product>> ListByCategoryIdAsync(Guid categoryId, CancellationToken cancellationToken);
+    Task RemoveAsync(Product product, CancellationToken cancellationToken);
+    Task RemoveRangeAsync(List<Product> products, CancellationToken cancellationToken);
+    Task UpdateAsync(Product product, CancellationToken cancellationToken);
+}
