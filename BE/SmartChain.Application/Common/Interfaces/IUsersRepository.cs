80bb39d62ec2a0d49aa01b93bd062244b60c172d
vanminh1878 <vanminh130804@gmail.com>
2025-05-23 21:53:53 +0700

    BE: Xây IUsersRepository

---
+++ BE/SmartChain.Application/Common/Interfaces/IUsersRepository.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using SmartChain.Domain.User;
+
+namespace SmartChain.Application.Common.Interfaces;
+
+public interface IUsersRepository
+{
+    Task AddAsync(User user, CancellationToken cancellationToken);
+    Task<User?> GetByIdAsync(Guid userId, CancellationToken cancellationToken);
+    Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken);
+    Task<User?> GetByAccountIdAsync(Guid accountId, CancellationToken cancellationToken);
+    Task<List<User>> ListByRoleIdAsync(Guid roleId, CancellationToken cancellationToken);
+    Task RemoveAsync(User user, CancellationToken cancellationToken);
+    Task RemoveRangeAsync(List<User> users, CancellationToken cancellationToken);
+    Task UpdateAsync(User user, CancellationToken cancellationToken);
+}
--- BE/SmartChain.Application/obj/Debug/net6.0/SmartChain.Application.AssemblyInfo.cs
+++ BE/SmartChain.Application/obj/Debug/net6.0/SmartChain.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartChain.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+158c5d303374f05167a8d92ebb64afc62c6bc2d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59adabcf08f3c7515dfb2eb778ceeaa9a39ad888")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartChain.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartChain.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
