5d764d2726c2cb8b96d4abba8c2020aad385bb84
vanminh1878 <vanminh130804@gmail.com>
2025-05-23 21:48:10 +0700

    BE: Xây IEmployeesRepository

---
+++ BE/SmartChain.Application/Common/Interfaces/IEmployeesRepository.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using SmartChain.Domain.Employee;
+
+namespace SmartChain.Application.Common.Interfaces;
+
+public interface IEmployeesRepository
+{
+    Task AddAsync(Employee employee, CancellationToken cancellationToken);
+    Task<Employee?> GetByIdAsync(Guid employeeId, CancellationToken cancellationToken);
+    Task<Employee?> GetByUserIdAsync(Guid userId, CancellationToken cancellationToken);
+    Task<List<Employee>> ListByStoreIdAsync(Guid storeId, CancellationToken cancellationToken);
+    Task<List<Employee>> ListByStatusAsync(bool status, CancellationToken cancellationToken);
+    Task RemoveAsync(Employee employee, CancellationToken cancellationToken);
+    Task RemoveRangeAsync(List<Employee> employees, CancellationToken cancellationToken);
+    Task UpdateAsync(Employee employee, CancellationToken cancellationToken);
+}
--- BE/SmartChain.Application/obj/Debug/net6.0/SmartChain.Application.AssemblyInfo.cs
+++ BE/SmartChain.Application/obj/Debug/net6.0/SmartChain.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartChain.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0cf077968d638fee1572bc878ad9ed4a5688d6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+512e66881ac31bccadbfc4e78a8a83daef9ff941")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartChain.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartChain.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
